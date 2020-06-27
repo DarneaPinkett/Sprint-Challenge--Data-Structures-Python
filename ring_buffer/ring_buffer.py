371fecfc444c7afde36fe121d4bdf78999153752
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-06-27 12:22:13 -0400

    ringbuffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
 
     def append(self, item):
-        pass
+        self.storage.append(item)
+        if len(self.storage) == self.capacity:
+            self.cur = 0
+            self.__class__ = self.__Full
 
     def get(self):
-        pass
+        return self.storage
+
+    class __Full:
+        def append(self, x):
+            self.storage[self.cur] = x
+            self.cur = (self.cur+1) % self.capacity
+
+        def get(self):
+            return self.storage
